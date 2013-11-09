5b5e072f3cdb91d223cc52424d66878798590720
p214c <p_cesario@hotmail.com>
2013-11-09 13:59:54 -0700

    updated to serve static webapp files along with REST api

--- src/main/server/server.js
+++ src/main/server/server.js
@@ -1,10 +1,29 @@
-var express = require('express');
-
-var exp = express();
+var express = require("express");
+var path = require("path");
 var notes = require('./routes/notes');
 
-exp.get('/notes', notes.findAll);
-exp.get('/notes/:id', notes.findById);
+var application_root = __dirname;
+var webapp_root = path.join(application_root, '../webapp');
+var oneDay = 86400000;
 
-exp.listen(8080);
+var app = express();
+
+// app configure
+app.use(express.compress());
+
+app.use(express.errorHandler({
+  dumpExceptions : true,
+  showStack : true
+}));
+
+// static files
+app.use('/cnote', express.static(webapp_root), {
+  maxAge : oneDay
+});
+
+// REST
+app.get('/notes', notes.findAll);
+app.get('/notes/:id', notes.findById);
+
+app.listen(8080);
 console.log('Listening on port 8080...');
