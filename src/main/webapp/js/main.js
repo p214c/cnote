a817553d943eba662481f118b162aef0219d58ba
p214c <p_cesario@hotmail.com>
2013-11-09 19:34:14 -0700

    fixed initial load of bootstrap

--- src/main/webapp/js/main.js
+++ src/main/webapp/js/main.js
@@ -1,36 +1,38 @@
 require.config({
   // Sets the js folder as the base directory for all future relative paths
-  baseUrl: './js',
-  paths: {
-      // core libraries
-      jquery: 'vendor/jquery-1.10.1.min',
-//      'underscore': 'vendor/lodash',
+  baseUrl : 'js',
+  paths : {
+    // core libraries
+    jquery : 'vendor/jquery-1.10.1.min',
+    // 'underscore': 'vendor/lodash',
 
-      // plugins
-      bootstrap: 'vendor/bootstrap.min.js'
+    // plugins
+    bootstrap : 'vendor/bootstrap.min'
 
-      // application folders
-//      collections: 'app/collections',
-//      models: 'app/models',
-//      routers: 'app/routers',
-//      templates: 'app/templates',
-//      views: 'app/views',
-//      layouts: 'app/layouts',
-//      configs: 'app/config'
+  // application folders
+  // collections: 'app/collections',
+  // models: 'app/models',
+  // routers: 'app/routers',
+  // templates: 'app/templates',
+  // views: 'app/views',
+  // layouts: 'app/layouts',
+  // configs: 'app/config'
 
   },
 
   // sets the configuration for your AMD incompatible third party scripts
-  shim: {
-      bootstrap: ['jquery']
-//      backbone: {
-//        // Depends on underscore/lodash and jQuery
-//        'deps': ['underscore', 'jquery'],
-//        // Exports the global window.Backbone object
-//        'exports': 'Backbone'
-//      },
+  shim : {
+    bootstrap : [ 'jquery' ]
+  // backbone: {
+  // // Depends on underscore/lodash and jQuery
+  // 'deps': ['underscore', 'jquery'],
+  // // Exports the global window.Backbone object
+  // 'exports': 'Backbone'
+  // },
   },
 
-  enforceDefine: true
+});
 
+// force load of bootstrap
+require([ 'jquery', 'bootstrap' ], function($) {
 });
