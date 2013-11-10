3c17a9637a0ea8bf651b71093132810e963d943d
p214c <p_cesario@hotmail.com>
2013-11-09 18:30:01 -0700

    adding requirejs

--- src/main/webapp/js/main.js
+++ src/main/webapp/js/main.js
@@ -1,1 +1,36 @@
+require.config({
+  // Sets the js folder as the base directory for all future relative paths
+  baseUrl: './js',
+  paths: {
+      // core libraries
+      jquery: 'vendor/jquery-1.10.1.min',
+//      'underscore': 'vendor/lodash',
 
+      // plugins
+      bootstrap: 'vendor/bootstrap.min.js'
+
+      // application folders
+//      collections: 'app/collections',
+//      models: 'app/models',
+//      routers: 'app/routers',
+//      templates: 'app/templates',
+//      views: 'app/views',
+//      layouts: 'app/layouts',
+//      configs: 'app/config'
+
+  },
+
+  // sets the configuration for your AMD incompatible third party scripts
+  shim: {
+      bootstrap: ['jquery']
+//      backbone: {
+//        // Depends on underscore/lodash and jQuery
+//        'deps': ['underscore', 'jquery'],
+//        // Exports the global window.Backbone object
+//        'exports': 'Backbone'
+//      },
+  },
+
+  enforceDefine: true
+
+});
---
+++ src/main/webapp/js/wrappers.js
@@ -0,0 +1,3 @@
+define('modernizr', function() {
+  return window.Modernizr;
+});
