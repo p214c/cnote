0e63ece7e0b733707a65b31297bded40fbece1bb
p214c <p_cesario@hotmail.com>
2013-11-17 11:16:12 -0700

    adding fixed footer

--- src/main/webapp/js/views/MainView.js
+++ src/main/webapp/js/views/MainView.js
@@ -3,27 +3,30 @@ define([ 'text!templates/main-header-container.htm',
     'text!templates/main-header-collapse.htm',
     'text!templates/main-content-container.htm',
     'text!templates/main-content-row-item.htm',
-    'text!templates/main-footer-container.htm', 'jquery' ], function(
+    'text!templates/main-footer-container.htm',
+    'text!templates/main-footer-bar.htm', 'jquery' ], function(
     hdrContainer, hdrBar, hdrCollapse, contentContainer, contentRowItem,
-    ftrContainer) {
+    ftrContainer, ftrBar) {
   function MainView() {
 
     function load() {
-      var $body = $('body');
+      var $bodyContainer = $('body>.body-wrapper>.body-container');
       var $hdrContainer = $(hdrContainer);
-      $body.append($hdrContainer);
-      var $hdr = $hdrContainer.children('.container:first-child');
+      $bodyContainer.append($hdrContainer);
+      var $hdr = $hdrContainer.children('.header-container:first-child');
       $hdr.append(hdrBar);
       $hdr.append(hdrCollapse);
 
       var $contentContainer = $(contentContainer);
-      $body.append($contentContainer);
+      $bodyContainer.append($contentContainer);
       var $content = $contentContainer.children('.row:first-child');
       for (var i = 0; i < 3; i++) {
         $content.append(contentRowItem);
       }
 
-      $body.append(ftrContainer);
+      var $ftr = $(ftrContainer);
+      $bodyContainer.append($ftr);
+      $ftr.append(ftrBar);
     }
     
     function init() {
