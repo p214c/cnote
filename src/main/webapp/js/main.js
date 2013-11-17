0e548c8882b0a176a66785bd78aad04cadc0378a
p214c <p_cesario@hotmail.com>
2013-11-17 16:03:11 -0700

    added wysihtml5 as editor body

--- src/main/webapp/js/main.js
+++ src/main/webapp/js/main.js
@@ -3,12 +3,14 @@ require.config({
   baseUrl : 'js',
   paths : {
     // core libraries
-    jquery : 'vendor/jquery-1.10.1.min',
+    jquery : 'vendor/jquery-1.10.2.min',
     // 'underscore': 'vendor/lodash',
 
     // plugins
-    bootstrap : 'vendor/bootstrap.min',
     text : 'vendor/text',
+    bootstrap : 'vendor/bootstrap.min',
+    wysihtml5 : 'vendor/wysihtml5-0.3.0.min',
+    'bootstrap-wysihtml5' : 'vendor/bootstrap-wysihtml5-0.0.3.min',
 
     // application folders
     // collections: 'app/collections',
@@ -23,7 +25,9 @@ require.config({
 
   // sets the configuration for your AMD incompatible third party scripts
   shim : {
-    bootstrap : [ 'jquery' ]
+    bootstrap : [ 'jquery' ],
+    wysihtml5 : [ 'jquery' ],
+    'bootstrap-wysihtml5' : [ 'jquery', 'bootstrap', 'wysihtml5' ],
   // backbone: {
   // // Depends on underscore/lodash and jQuery
   // 'deps': ['underscore', 'jquery'],
--- src/main/webapp/js/views/MainView.js
+++ src/main/webapp/js/views/MainView.js
@@ -1,55 +1,61 @@
-define([ 'text!templates/main-header-container.htm',
-    'text!templates/main-header-bar.htm',
-    'text!templates/main-header-collapse.htm',
-    'text!templates/main-content-container.htm',
-    'text!templates/main-content-row-item.htm',
-    'text!templates/main-footer-container.htm', 'presenters/NotePresenter',
-    'jquery' ], function(hdrContainer, hdrBar, hdrCollapse, contentContainer,
-    contentRowItem, ftrContainer, NotePresenter) {
-  function MainView() {
+define(
+    [ 'text!templates/main-header-container.htm',
+        'text!templates/main-header-bar.htm',
+        'text!templates/main-header-collapse.htm',
+        'text!templates/main-content-container.htm',
+        'text!templates/main-content-row-item.htm',
+        'text!templates/main-footer-container.htm', 'presenters/NotePresenter',
+        'bootstrap-wysihtml5', 'jquery' ],
+    function(hdrContainer, hdrBar, hdrCollapse, contentContainer,
+        contentRowItem, ftrContainer, NotePresenter) {
+      function MainView() {
 
-    function addHeader(parent) {
-      var $hdrContainer = $(hdrContainer);
-      parent.append($hdrContainer);
-      var $hdr = $hdrContainer.children('.header-container:first-child');
-      $hdr.append(hdrBar);
-      $hdr.append(hdrCollapse);
-    }
+        function addHeader(parent) {
+          var $hdrContainer = $(hdrContainer);
+          parent.append($hdrContainer);
+          var $hdr = $hdrContainer.children('.header-container:first-child');
+          $hdr.append(hdrBar);
+          $hdr.append(hdrCollapse);
+        }
 
-    function addContent(parent) {
-      var $contentContainer = $(contentContainer);
-      parent.append($contentContainer);
-      var $content = $contentContainer.children('.row:first-child');
-      for (var i = 0; i < 3; i++) {
-        $content.append(contentRowItem);
+        function addContent(parent) {
+          var $contentContainer = $(contentContainer);
+          parent.append($contentContainer);
+          var $content = $contentContainer.children('.row:first-child');
+
+          var $contentRowItem = $(contentRowItem);
+          $content.append($contentRowItem);
+
+          $contentRowItem.children('textarea:first-child').wysihtml5({
+            stylesheets : [ "css/wysiwyg-color.css" ]
+          });
+        }
+
+        function addFooter(parent) {
+          var $ftr = $(ftrContainer);
+          parent.append($ftr);
+
+          // TODO add Resource.get handling for i18n
+          var btnHtml = '<a class="btn btn-default btn-store" href="#">Store</a>';
+          var $storeButton = $(btnHtml);
+          $ftr.append($storeButton);
+
+          $storeButton.on('click', NotePresenter.storeNote);
+        }
+
+        function load() {
+          var $bodyContainer = $('body>.body-wrapper>.body-container');
+          addHeader($bodyContainer);
+          addContent($bodyContainer);
+          addFooter($bodyContainer);
+        }
+
+        function init() {
+          $(document).ready(load);
+        }
+        this.init = init;
+
       }
-    }
 
-    function addFooter(parent) {
-      var $ftr = $(ftrContainer);
-      parent.append($ftr);
-
-      // TODO add Resource.get handling for i18n
-      var btnHtml = '<a class="btn btn-default btn-store" href="#">Store</a>';
-      var $storeButton = $(btnHtml);
-      $ftr.append($storeButton);
-
-      $storeButton.on('click', NotePresenter.storeNote);
-    }
-
-    function load() {
-      var $bodyContainer = $('body>.body-wrapper>.body-container');
-      addHeader($bodyContainer);
-      addContent($bodyContainer);
-      addFooter($bodyContainer);
-    }
-
-    function init() {
-      $(document).ready(load);
-    }
-    this.init = init;
-
-  }
-
-  return new MainView();
-});
+      return new MainView();
+    });
