c9ac42eacf8be76502f7d58760bbbe15530d4964
p214c <p_cesario@hotmail.com>
2013-11-20 21:03:43 -0700

    switched to https only on port 8443

--- src/main/server/server.js
+++ src/main/server/server.js
@@ -1,5 +1,9 @@
 var express = require("express");
 var path = require("path");
+var fs = require('fs');
+var http = require('http');
+var https = require('https');
+
 var notes = require('./routes/notes');
 
 var application_root = __dirname;
@@ -10,6 +14,7 @@ function logErrors(err, req, res, next) {
   console.error(err.stack);
   next(err);
 }
+
 function clientErrorHandler(err, req, res, next) {
   if (req.xhr) {
     res.send(500, {
@@ -46,5 +51,18 @@ app.get('/notes/:id', notes.findById);
 app.post('/notes', notes.add);
 app.put('/notes/:id', notes.update);
 
-app.listen(8080);
-console.log('Listening on port 8080...');
+// self signed cert and key generated with openssl -
+// http://stackoverflow.com/questions/10175812/how-to-build-a-self-signed-certificate-with-openssl
+// openssl req -x509 -newkey -nodes rsa:2048 -keyout key.pem -out cert.pem -days XXX
+var privateKey = fs.readFileSync('sslcert/key.pem', 'utf8');
+var certificate = fs.readFileSync('sslcert/cert.pem', 'utf8');
+var credentials = {
+  key : privateKey,
+  cert : certificate
+};
+// var httpServer = http.createServer(app);
+var httpsServer = https.createServer(credentials, app);
+
+// httpServer.listen(8080);
+httpsServer.listen(8443);
+console.log('Listening on port 8443...');
