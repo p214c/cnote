b79ad4e4c154ba9dd5aca8f882fe218ba28e26d0
p214c <p_cesario@hotmail.com>
2014-01-05 08:33:20 -0700

    merging phonegap generated example project files into the cnote application

--- cnote-app/src/main/webapp/js/app.js
+++ cnote-app/src/main/webapp/js/app.js
@@ -1,9 +1,42 @@
-define([ 'jquery', 'views/MainView'], function($, MainView) {
-	var initialize = function() {
-		MainView.init();
-	};
+define([ 'jquery', 'views/MainView' ], function($, MainView) {
+  var me = this;
 
-	return {
-		initialize : initialize
-	};
+  // Update DOM on a Received Event
+  function receivedEvent(id) {
+    var parentElement = document.getElementById(id);
+    var listeningElement = parentElement.querySelector('.listening');
+    var receivedElement = parentElement.querySelector('.received');
+
+    listeningElement.setAttribute('style', 'display:none;');
+    receivedElement.setAttribute('style', 'display:block;');
+
+    console.log('Received Event: ' + id);
+  }
+  this.receivedEvent = receivedEvent;
+
+  // deviceready Event Handler
+  //
+  // The scope of 'this' is the event. In order to call the 'receivedEvent'
+  // function, we must explicity call 'app.receivedEvent(...);'
+  function onDeviceReady() {
+    me.receivedEvent('deviceready');
+    MainView.init();
+  }
+  this.onDeviceReady = onDeviceReady;
+
+  // Bind Event Listeners
+  //
+  // Bind any events that are required on startup. Common events are:
+  // 'load', 'deviceready', 'offline', and 'online'.
+  function bindEvents() {
+    document.addEventListener('deviceready', this.onDeviceReady, false);
+  }
+
+  var initialize = function() {
+    bindEvents();
+  };
+
+  return {
+    initialize : initialize
+  };
 });
