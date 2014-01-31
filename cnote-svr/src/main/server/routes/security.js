9d5edee0828e80fa2571bd8293c8ad29818e2e0b
p214c <p_cesario@hotmail.com>
2014-01-30 21:38:51 -0700

    removed extra db code that was not intended for prior checkin

--- cnote-svr/src/main/server/routes/security.js
+++ cnote-svr/src/main/server/routes/security.js
@@ -2,20 +2,6 @@ var Security = function() {
   var passport = require('passport');
   var LocalStrategy = require('passport-local').Strategy;
 
-  passport.use(new LocalStrategy(function(username, password,done){
-    User.findOne({ username : username},function(err,user){
-        if(err) { return done(err); }
-        if(!user){
-            return done(null, false, { message: 'Incorrect username.' });
-        }
-
-        hash( password, user.salt, function (err, hash) {
-            if (err) { return done(err); }
-            if (hash == user.hash) return done(null, user);
-            done(null, false, { message: 'Incorrect password.' });
-        });
-    });
-}));  
   function initLocalStrategy() {
     passport.use(new LocalStrategy(function(username, password, done) {
       User.findOne({
