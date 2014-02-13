9c45b18a3a69ad14568cfa444002f5e94359c34f
Paul Cesario <p_cesario@hotmail.com>
2014-02-12 19:10:54 -0700

    attempting to structure the notes queries better

--- cnote-svr/src/main/server/routes/notesRoute.js
+++ cnote-svr/src/main/server/routes/notesRoute.js
@@ -1,89 +1,103 @@
 var NotesRoute = function() {
-  var _ = require('lodash');
-  var User = require('../models/users').User;
+	var _ = require('lodash');
+	var User = require('../models/users').User;
 
-  var note = function(id) {
-    var d = new Date();
+	var note = function(id) {
+		var d = new Date();
 
-    this.id = id;
-    this.title = 'title';
-    this.description = 'description';
-    this.cabinet = 'cabinet';
-    this.drawer = 'drawer';
-    this.data = 'data';
-    this.keywords = 'keywords';
-    this.created = d.getTime();
-    this['last-modified'] = d.getTime();
-  };
+		this.id = id;
+		this.title = 'title';
+		this.description = 'description';
+		this.cabinet = 'cabinet';
+		this.drawer = 'drawer';
+		this.data = 'data';
+		this.keywords = 'keywords';
+		this.created = d.getTime();
+		this['last-modified'] = d.getTime();
+	};
 
-  function clearImmutableFields(data) {
-    delete data.id;
-    delete data.created;
-    delete data['last-modified'];
+	function clearImmutableFields(data) {
+		delete data.id;
+		delete data.created;
+		delete data['last-modified'];
 
-    return data;
-  }
+		return data;
+	}
 
-  function getNotes(req, res) {
-    User.findOne({
-      email : req.user.email
-    }, function(err, user) {
-      if (err) {
-        throw err;
-      }
+	function queryUser(email, handler) {
+		User.findOne({
+			email : email
+		}, handler);
 
-      if (user) {
-        var ns = user.notes;
-        if (req.query.ids) {
-          // only return the id and title
-          var nsIds = [];
-          for (var j = 0, jlen = ns.length; j < jlen; j++) {
-            nsIds.push({
-              id : ns[j]._id,
-              title : ns[j].title
-            });
-          }
+	}
 
-          ns = nsIds;
-        }
+	function queryHandler(err, user, data) {
+		if (err) {
+			throw err;
+		}
 
-        res.send(ns);
-      } else {
-        throw $.error('Unknown user.');
-      }
-    });
-  }
-  this.getNotes = getNotes;
+		if (user) {
+			data.success();
+		} else {
+			throw $.error('Unknown user.');
+		}
+	}
 
-  function getNote(id) {
-    if (!id) {
-      return '';
-    }
-    return notes[id - 1];
-  }
-  this.getNote = getNote;
+	function notesResponse() {
+		var ns = user.notes;
+		if (req.query.ids) {
+			// only return the id and title
+			var nsIds = [];
+			for (var j = 0, jlen = ns.length; j < jlen; j++) {
+				nsIds.push({
+					id : ns[j]._id,
+					title : ns[j].title
+				});
+			}
 
-  function addNote(data) {
-    var n = new note(notes.length + 1);
-    n.title += "-" + (notes.length + 1);
-    _.assign(n, clearImmutableFields(data));
+			ns = nsIds;
+		}
 
-    notes.push(n);
+		this.response.send(ns);
+	}
 
-    return n;
-  }
-  this.addNote = addNote;
+	function getNotes(req, res) {
+		queryUser(req.user.email, getNotesQueryHandler, {
+			success : notesResponse,
+			response : res
+		});
+	}
+	this.getNotes = getNotes;
 
-  function updateNote(id, data) {
-    var n = notes[id - 1];
-    if (n) {
-      _.assign(n, clearImmutableFields(data));
-      n['last-modified'] = new Date().getTime();
-    }
+	function getNote(id) {
+		if (!id) {
+			return '';
+		}
+		return notes[id - 1];
+	}
+	this.getNote = getNote;
 
-    return n;
-  }
-  this.updateNote = updateNote;
+	function addNote(data) {
+		var n = new note(notes.length + 1);
+		n.title += "-" + (notes.length + 1);
+		_.assign(n, clearImmutableFields(data));
+
+		notes.push(n);
+
+		return n;
+	}
+	this.addNote = addNote;
+
+	function updateNote(id, data) {
+		var n = notes[id - 1];
+		if (n) {
+			_.assign(n, clearImmutableFields(data));
+			n['last-modified'] = new Date().getTime();
+		}
+
+		return n;
+	}
+	this.updateNote = updateNote;
 
 };
 
@@ -92,13 +106,13 @@ var instance = new NotesRoute();
 exports.findAll = instance.getNotes;
 
 exports.findById = function(req, res) {
-  res.send(instance.getNote(req.params.id));
+	res.send(instance.getNote(req.params.id));
 };
 
 exports.add = function(req, res) {
-  res.send(instance.addNote(req.body));
+	res.send(instance.addNote(req.body));
 };
 
 exports.update = function(req, res) {
-  res.send(instance.updateNote(req.params.id, req.body));
+	res.send(instance.updateNote(req.params.id, req.body));
 };
