25a0d708226e9f6f83122ab20a6a9367dfe0c58b
p214c <p_cesario@hotmail.com>
2014-03-15 19:26:04 -0600

    fixing crypt using main test

--- cnote-plugins/encrypt-chrome/src/main/include/crypt.h
+++ cnote-plugins/encrypt-chrome/src/main/include/crypt.h
@@ -42,36 +42,35 @@ public:
 	// @return The decrypted value
 	std::string decrypt(std::string decipher);
 
-	int main() {
-		std::cout << "Starting";
-
-		crypto::Crypt* cipher = new crypto::Crypt();
-
-		std::string ENCRYPT_PREFIX = "encrypted||||";
-
-		std::string message = "encrypt this!";
-		std::cout << "Before encrypt: " + message;
-		message = "encrypted||||" + cipher->encrypt(message);
-		std::cout << "Encrypted: " + message;
-
-		if (message.find(ENCRYPT_PREFIX) > 0) {
-			std::cout << "Before decrypt: " + message;
-			message = cipher->decrypt(
-					message.substr(
-							message.find(ENCRYPT_PREFIX)
-									+ ENCRYPT_PREFIX.size()));
-			std::cout << "Decrypted: " + message;
-		} else {
-			std::cout << "Didn't detect encryption: " + message;
-			return 1;
-		}
-
-		std::cout << "Exiting";
-
-		return 0;
-	}
-
 };
 }  // namespace crypt
+
+int main() {
+	std::cout << "Starting";
+
+	crypto::Crypt* cipher = new crypto::Crypt();
+
+	std::string ENCRYPT_PREFIX = "\nencrypted||||";
+
+	std::string message = "encrypt this!";
+	std::cout << "\nBefore encrypt: " + message;
+	message = "encrypted||||" + cipher->encrypt(message);
+	std::cout << "\nEncrypted: " + message;
+
+	if (message.find(ENCRYPT_PREFIX) > 0) {
+		std::cout << "\nBefore decrypt: " + message;
+		message = cipher->decrypt(
+				message.substr(
+						message.find(ENCRYPT_PREFIX) + ENCRYPT_PREFIX.size()));
+		std::cout << "\nDecrypted: " + message;
+	} else {
+		std::cout << "\nDidn't detect encryption: " + message;
+		return 1;
+	}
+
+	std::cout << "\nExiting\n";
+
+	return 0;
+}
 
 #endif  // CRYPT_CPP_MODULE_H_
